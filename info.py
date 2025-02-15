2a33b26a436275333323b73b78e99eca5c509312
erfgdgdgfd <fgdhfghfgdfgdfhgf@gmail.com>
2025-02-16 01:44:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,10 +76,10 @@ TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_to_Download_7x/32') # Tutor
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 
 #Channel & Group link 
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie7xchat')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/nova_flix')
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/cosmic_freak')
-MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/nova_flix')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+goZZ0Oudd0owMzdl')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+7xZsDwzKX5UwM2Nl')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/+goZZ0Oudd0owMzdl')
+MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/+goZZ0Oudd0owMzdl')
 
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
